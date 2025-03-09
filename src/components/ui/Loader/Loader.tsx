f538bf0cca97107568d027f9dbd603076a400875
Kostia Boiko <kotya.boko@gmail.com>
2025-03-09 17:46:52 +0200

    fix loader icon import path

--- src/components/ui/Loader/Loader.tsx
+++ src/components/ui/Loader/Loader.tsx
@@ -1,5 +1,5 @@
 import classes from './loader.module.scss'
-import Loader1 from '@/assets/icons/loadingSpinner.svg?react'
+import Loader1 from '@/assets/icons/loading-spinner.svg?react'
 const Loader = () => {
   return (
       <Loader1 className={classes.loader}/>

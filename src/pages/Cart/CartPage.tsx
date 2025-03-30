941982660d074b5a607c7fb3a0fa53a779b7db06
Kostia Boiko <kotya.boko@gmail.com>
2025-03-30 23:59:12 +0300

    add responsive styles to Cart

--- src/pages/Cart/CartPage.tsx
+++ src/pages/Cart/CartPage.tsx
@@ -11,11 +11,13 @@ import EmptyCart from "@/features/cart/components/EmptyCart/EmptyCart";
 import { useAppDispatch, useAppSelector } from "@/redux/types";
 import { clearCartLocal } from "@/features/cart/slices/cartSlice";
 const CartPage = () => {
-  const isAuth = useAppSelector(state => state.auth.isAuth);
-  const [clearCart, {isLoading}] = useClearCartMutation();
-  const { data } = useGetCartQuery(undefined, {skip: isAuth ? false : true});
-  const dataLocal = useAppSelector(state => state.cart.productsInCartNoAuthUser);
-  const dispatch = useAppDispatch()
+  const isAuth = useAppSelector((state) => state.auth.isAuth);
+  const [clearCart, { isLoading }] = useClearCartMutation();
+  const { data } = useGetCartQuery(undefined, { skip: isAuth ? false : true });
+  const dataLocal = useAppSelector(
+    (state) => state.cart.productsInCartNoAuthUser
+  );
+  const dispatch = useAppDispatch();
   return (
     <div className={classes.cart}>
       <div className="wrapper">
@@ -23,22 +25,22 @@ const CartPage = () => {
         <div className={classes.cart__container}>
           {(dataLocal.length && !isAuth) || data?.length ? (
             <>
-              <div className={classes.cart__products}>
-                <CartProducts />
-              </div>
-              <div>
+              <CartProducts />
+              <div className={classes.cart__delivery}>
                 <DeliveryInfo />
                 <MainButton
-                  action={() => isAuth ? clearCart() : dispatch(clearCartLocal())}
+                  action={() =>
+                    isAuth ? clearCart() : dispatch(clearCartLocal())
+                  }
                   width="full"
                   className={classes.cart__clear}
                 >
-                  {isLoading ? <Loader/> : 'Clear Cart'}
+                  {isLoading ? <Loader /> : "Clear Cart"}
                 </MainButton>
               </div>
             </>
           ) : (
-            <EmptyCart/>
+            <EmptyCart />
           )}
         </div>
       </div>

68d626f324a5fbc004ecdae38d7ced7940d38f95
Kostia Boiko <kotya.boko@gmail.com>
2025-04-16 22:44:36 +0300

    remove FadeSlider  add rollup-plugin-visualizer minify imgs

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import react from "@vitejs/plugin-react";
 import path from 'path';
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
+import { visualizer } from 'rollup-plugin-visualizer';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), visualizer({ open: true })],
   resolve: {
 		alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
 	},
